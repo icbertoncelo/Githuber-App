c17b6410834e80696c442d3577798ac6f889af48
Ian Carlos <carlos.ian007@gmail.com>
2019-05-14 16:36:53 -0300

    Style: Add welcome page

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '~/services/api';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text>Testing Routes</Text>
-  </View>
-);
+class Welcome extends Component {
+  state = {
+    username: '',
+  };
+
+  render() {
+    const { username } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.text}>Enter your Github username to continue</Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Type your username"
+            underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
+          />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {}}
+          >
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default Welcome;
