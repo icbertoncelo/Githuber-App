5a159cf37409d4321332b7e2c0bf84d185bb230a
Ian Carlos <carlos.ian007@gmail.com>
2019-05-14 20:46:40 -0300

    Style(tabs): Add BottomTabNavigator

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-// import { Container } from './styles';
+import Header from '~/components/Header';
 
-const Organizations = () => <View />;
+const Organizations = () => (
+  <View>
+    <Header title="Organizations" />
+  </View>
+);
+
+const TabIcon = ({ tintColor }) => (
+  <Icon
+    name="building"
+    size={20}
+    color={tintColor}
+  />
+);
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import Header from '~/components/Header';
 
 const Repositories = () => (
@@ -8,5 +11,21 @@ const Repositories = () => (
     <Header title="Repositories" />
   </View>
 );
+
+const TabIcon = ({ tintColor }) => (
+  <Icon
+    name="list-alt"
+    size={20}
+    color={tintColor}
+  />
+);
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Repositories.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Repositories;
