6d35eef89f3dc64ed9e5089fc425cca90587e621
Ian Carlos <carlos.ian007@gmail.com>
2019-05-13 22:05:45 -0300

    Feat(navigation): Add react-navigation and pages

--- src/index.js
+++ src/index.js
@@ -2,26 +2,8 @@ import './config/ReactotronConfig';
 import './config/DevToolsConfig';
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './routes';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    margin: 10,
-    textAlign: 'center',
-  },
-});
-
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Main Structure</Text>
-  </View>
-);
+const App = () => <Routes />;
 
 export default App;
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+const Welcome = () => (
+  <View>
+    <Text>Testing Routes</Text>
+  </View>
+);
+
+export default Welcome;
